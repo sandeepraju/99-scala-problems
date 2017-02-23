6ac465c7a9f116115341902192938fb5d9875970
Sandeep Raju Prabhakar <me@sandeepraju.in>
2017-02-22 20:32:18 -0800

    adds p02 solution

---
+++ src/main/scala/problems/p02/P02.scala
@@ -0,0 +1,44 @@
+// references:
+// 1. http://aperiodic.net/phil/scala/s-99/p02.scala
+// 2. http://alvinalexander.com/scala/how-to-extract-sequence-elements-collection-scala-cookbook
+
+package problems.p02
+
+import org.scalatest.Assertions._
+
+object P02 {
+  def main(args: Array[String]): Unit = {
+    // case 01
+    assert(
+      penultimate(List(1, 2, 3)) == 2,
+      "`penultimate` of valid list should return the element one away from last in the list"
+    )
+
+    // case 02
+    assertThrows[NoSuchElementException] {
+      penultimate(List(1))  // `penultimate` should throw an exception when the list has one item
+    }
+
+    // case 03
+    assert(
+      penultimate(List("one", "two", "three")) == "two",
+      "`penultimate` should work as expected on List[String] as well"
+    )
+
+    // case 04
+    assertThrows[NoSuchElementException] {
+      penultimate(null)  // `penultimate` should throw an exception when the list is null
+    }
+
+    // case 05
+    assertThrows[NoSuchElementException] {
+      penultimate(Nil)  // `penultimate` should throw an exception when the list is Nil
+    }
+  }
+
+  def penultimate[A](l: List[A]): A = l match {
+    case head :: _ :: Nil => head
+    case _  :: tail => penultimate(tail)
+    case _ => throw new NoSuchElementException
+  }
+}
---
+++ src/main/scala/problems/p02/README.md
@@ -0,0 +1,9 @@
+P02. Find the last but one element of a list
+============================================
+
+## Example
+
+``` scala
+scala> penultimate(List(1, 1, 2, 3, 5, 8))
+res0: Int = 5
+```
