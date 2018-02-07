d04475889b53e0473ab8b2cb181075ac4eacdf89
Sandeep Raju Prabhakar <me@sandeepraju.in>
2018-02-07 15:11:50 -0800

    adds tl;dr

--- src/main/scala/problems/p04/P04.scala
+++ src/main/scala/problems/p04/P04.scala
@@ -1,7 +1,11 @@
 // references:
 // 1. http://aperiodic.net/phil/scala/s-99/p04.scala
+//
 // NOTE: The above link has a very interesting comment
 // about tail recursive optimization in scala
+//
+// TL;DR: scala will do tail recursion optimization
+// if the recursive function is either local or final
 
 package problems.p04
 
