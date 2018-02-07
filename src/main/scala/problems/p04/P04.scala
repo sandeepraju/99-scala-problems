67683027d4a33085e94ba537aeefd57eb8da2d5d
Sandeep Raju Prabhakar <me@sandeepraju.in>
2018-02-07 13:51:07 -0800

    adds p04

---
+++ src/main/scala/problems/p04/P04.scala
@@ -0,0 +1,23 @@
+// references:
+// 1. http://aperiodic.net/phil/scala/s-99/p04.scala
+// NOTE: The above link has a very interesting comment
+// about tail recursive optimization in scala
+
+package problems.p04
+
+import org.scalatest.Assertions._
+
+object P04 {
+  def main(args: Array[String]): Unit = {
+    // case 01
+    assert(length(List()) == 0)
+
+    // case 02
+    assert(length(List(0, 1, 4)) == 3)
+  }
+
+  def length[A](ls: List[A]): Int = ls match {
+    case (Nil) => 0
+    case (_ :: tail) => 1 + length(tail)
+  }
+}
---
+++ src/main/scala/problems/p04/README.md
@@ -0,0 +1,9 @@
+P04. Find the number of elements of a list
+============================================
+
+## Example
+
+``` scala
+scala> length(List(1, 1, 2, 3, 5, 8))
+res0: Int = 6
+```
