56dc5622f3f9c090aa1dd2b03f7fc798ea6de460
Sandeep Raju Prabhakar <me@sandeepraju.in>
2018-02-17 01:48:42 -0800

    p15

---
+++ src/main/scala/problems/p15/P15.scala
@@ -0,0 +1,30 @@
+// references:
+// 1. http://aperiodic.net/phil/scala/s-99/p15.scala
+
+package problems.p15
+
+import org.scalatest.Assertions._
+
+import scala.annotation.tailrec
+
+object P15 {
+  def main(args: Array[String]): Unit = {
+    // case 01
+    assert(duplicateN(3, List('a, 'b, 'c, 'c, 'd)) ==
+      List('a, 'a, 'a, 'b, 'b, 'b, 'c, 'c, 'c, 'c, 'c, 'c, 'd, 'd, 'd))
+
+    // case 02
+    assert(duplicateN(1, List('a)) == List('a))
+
+    // case 03
+    assert(duplicateN(2, List()) == List())
+
+    // case 04
+    assert(duplicateN(0, List('a)) == List())
+
+    // case 05
+    assert(duplicateN(2, List('a)) == List('a, 'a))
+  }
+
+  def duplicateN[A](n: Int, ls: List[A]): List[A] = ls.flatMap(x => List.fill(n)(x))
+}
---
+++ src/main/scala/problems/p15/README.md
@@ -0,0 +1,8 @@
+# P15. Duplicate the elements of a list a given number of times
+ 
+## Example
+
+``` scala
+scala> duplicateN(3, List('a, 'b, 'c, 'c, 'd))
+res0: List[Symbol] = List('a, 'a, 'a, 'b, 'b, 'b, 'c, 'c, 'c, 'c, 'c, 'c, 'd, 'd, 'd)
+```
