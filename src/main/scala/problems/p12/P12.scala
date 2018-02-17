ca1682d410a286a75b0bbe34021ac123f681fb4d
Sandeep Raju Prabhakar <me@sandeepraju.in>
2018-02-17 00:56:13 -0800

    p12

--- src/main/scala/problems/p12/P12.scala
+++ src/main/scala/problems/p12/P12.scala
@@ -7,7 +7,19 @@ import org.scalatest.Assertions._
 
 object P12 {
   def main(args: Array[String]): Unit = {
+    // case 01
+    assert(decode(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
+      == List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+
+    // case 02
+    assert(decode(List()) == List())
+
+    // case 03
+    assert(decode(List((4, 'a))) == List('a, 'a, 'a, 'a))
+
+    // case 04
+    assert(decode(List((1, 'a))) == List('a))
   }
 
-  def decode[A](ls: List[(Int, A)]): List[A] = ???
+  def decode[A](ls: List[(Int, A)]): List[A] = ls.flatMap(x => List.fill(x._1)(x._2))
 }
