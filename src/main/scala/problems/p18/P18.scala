2ead0c4ef7a6d88237fe8f9ec5378ac9888baf5a
Sandeep Raju Prabhakar <me@sandeepraju.in>
2018-02-19 00:57:42 -0800

    p18 stub

---
+++ src/main/scala/problems/p18/P18.scala
@@ -0,0 +1,10 @@
+// references:
+// 1. http://aperiodic.net/phil/scala/s-99/p18.scala
+
+package problems.p18
+
+object P18 {
+  def main(args: Array[String]): Unit = {}
+
+  def slice[A](start: Int, end: Int, ls: List[A]): List[A] = ???
+}
