b4c3a088a07a0f2d25704c659e067c01749e4e59
Sandeep Raju Prabhakar <me@sandeepraju.in>
2018-02-20 14:01:57 -0800

    adds test cases

--- src/main/scala/problems/p18/P18.scala
+++ src/main/scala/problems/p18/P18.scala
@@ -4,7 +4,48 @@
 package problems.p18
 
 object P18 {
-  def main(args: Array[String]): Unit = {}
+  def main(args: Array[String]): Unit = {
+    // case 01
+    assert(slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) ==
+      List('d, 'e, 'f, 'g))
 
-  def slice[A](start: Int, end: Int, ls: List[A]): List[A] = ???
+    // case 02
+    assert(slice(0, 0, List()) == List())
+
+    // case 03
+    assert(slice(0, 0, List()) == List())
+
+    // case 04
+    assert(slice(10, 0, List()) == List())
+
+    // case 05
+    assert(slice(10, 0, List(1, 2, 3)) == List())
+
+    // case 06
+    assert(slice(2, 2, List(1, 2, 3)) == List())
+
+    // case 07
+    assert(slice(2, 3, List(1, 2, 3)) == List(3))
+
+    // case 08
+    assert(slice(2, 4, List(1, 2, 3, 4, 5)) == List(3, 4))
+
+    // case 09
+    assert(slice(2, 40, List(1, 2, 3, 4, 5)) == List(3, 4, 5))
+
+    // case 10
+    assert(slice(-2, -40, List(1, 2, 3, 4, 5)) == List())
+
+  }
+
+  def slice[A](from: Int, until: Int, ls: List[A]): List[A] = {
+
+    // builtin
+    ls.slice(from, until)
+
+    // do prelim checks
+
+
+
+  }
 }
