3063a11de10b8bed4f6f27dd537149bb8d11f8ef
Sandeep Raju Prabhakar <me@sandeepraju.in>
2018-02-20 22:05:50 -0800

    p20

---
+++ src/main/scala/problems/p20/P20.scala
@@ -0,0 +1,35 @@
+// references:
+// 1. http://aperiodic.net/phil/scala/s-99/p20.scala
+// NOTE: The above link has a recursive implementation
+// which partially uses builtins.
+
+package problems.p19
+
+import java.util.NoSuchElementException
+import org.scalatest.Assertions._
+
+object P20 {
+
+  def main(args: Array[String]): Unit = {
+    // case 01
+    assert(removeAt(1, List('a, 'b, 'c, 'd)) == (List('a, 'c, 'd),'b))
+
+    // case 02
+    assertThrows[NoSuchElementException] {
+      removeAt(10, List(1, 2))
+    }
+
+    // case 03
+    assertThrows[NoSuchElementException] {
+      removeAt(-1, List(1, 2))
+    }
+
+  }
+
+  def removeAt[A](n: Int, ls: List[A]): (List[A], A) = {
+    if (n < 0 || n >= ls.length) throw new NoSuchElementException()
+
+    val (first, second) = ls.splitAt(n)
+    (first ::: second.tail, second.head)
+  }
+}
---
+++ src/main/scala/problems/p20/README.md
@@ -0,0 +1,10 @@
+# P20. Remove the Kth element from a list
+
+Return the list and the removed element in a Tuple. Elements are numbered from 0.
+
+## Example
+
+``` scala
+scala> removeAt(1, List('a, 'b, 'c, 'd))
+res0: (List[Symbol], Symbol) = (List('a, 'c, 'd),'b)
+```
