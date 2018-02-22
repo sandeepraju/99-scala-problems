9a845722ba622078296d201b03fae80cb097c5e9
Sandeep Raju Prabhakar <me@sandeepraju.in>
2018-02-21 22:20:54 -0800

    p22 unfoldr implementation

--- src/main/scala/problems/p22/P22.scala
+++ src/main/scala/problems/p22/P22.scala
@@ -9,20 +9,37 @@ object P22 {
   def main(args: Array[String]): Unit = {
     // case 01
     assert(range(4, 9) == List(4, 5, 6, 7, 8, 9))
+
+    // case 02
+    assert(range(1, 1) == List(1))
+
+    // case 03
+    assert(range(2, 1) == List())
+
+    // case 04
+    assert(range(1, 2) == List(1, 2))
+
   }
 
   def range(start: Int, end: Int): List[Int] = {
     // builtin 1
-    start to end toList
+    // start to end toList
 
     // builtin 2
-    List.range(start, end)
+    // List.range(start, end)
 
     // Functional way using "unfold right"
     // NOTES:
     // - fold function folds a iterable (accumulates over the list with one element at a time)
     // - unfold takes a accumulation and generates a list (iterable)
-    def unfold() = ???
+    def unfoldr[A, B](seed: B)(f: B => Option[(A, B)]): List[A] = f(seed) match {
+      case None => Nil
+      case Some((result, nextSeed)) => result :: unfoldr(nextSeed)(f)
+    }
 
+    unfoldr(start) { n =>
+      if (n > end) None // We should end this list now
+      else Some((n, n + 1)) // Add the value of n to the list and increment the seed
+    }
   }
 }
