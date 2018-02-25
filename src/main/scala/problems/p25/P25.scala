4c2f5b55586cce3644ae6ffae3aaa6590c0824ad
Sandeep Raju Prabhakar <me@sandeepraju.in>
2018-02-25 02:08:55 -0800

    p25 stub

---
+++ src/main/scala/problems/p25/P25.scala
@@ -0,0 +1,16 @@
+// references:
+// 1. http://aperiodic.net/phil/scala/s-99/p25.scala
+
+package problems.p25
+
+import scala.util.Random
+
+object P25 {
+
+  Random.setSeed(1337)
+
+  def main(args: Array[String]): Unit = {
+  }
+
+  def randomPermute[A](ls: List[A]): List[A] = ???
+}
---
+++ src/main/scala/problems/p25/README.md
@@ -0,0 +1,10 @@
+# P25. Generate a random permutation of the elements of a list.
+
+Hint: Use the solution of problem [P23](../p23/README.md).
+
+## Example
+
+``` scala
+scala> randomPermute(List('a, 'b, 'c, 'd, 'e, 'f))
+res0: List[Symbol] = List('b, 'a, 'd, 'c, 'e, 'f)
+```
