f802d30367630e5583da92c8e5a894cf2516c8bc
Sandeep Raju Prabhakar <me@sandeepraju.in>
2018-02-25 13:03:41 -0800

    p25

--- src/main/scala/problems/p25/P25.scala
+++ src/main/scala/problems/p25/P25.scala
@@ -10,7 +10,22 @@ object P25 {
   Random.setSeed(1337)
 
   def main(args: Array[String]): Unit = {
+    // case 01
+    assert(randomPermute(List('a, 'b, 'c, 'd, 'e, 'f)) == List('a, 'e, 'c, 'd, 'f, 'b))
+
+    // case 02
+    assert(randomPermute(List()) == List())
+
+    // case 03
+    assert(randomPermute(List('a)) == List('a))
   }
 
-  def randomPermute[A](ls: List[A]): List[A] = ???
+  def randomPermute[A](ls: List[A]): List[A] = {
+    // Built-in
+    Random.shuffle(ls)
+
+    import problems.p23.P23
+
+    P23.randomSelect(ls.length, ls)
+  }
 }
