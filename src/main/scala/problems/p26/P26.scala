1f74cdbb5adee36439b88b84bb3bede8c51d06d8
Sandeep Raju Prabhakar <me@sandeepraju.in>
2018-02-26 22:57:43 -0800

    p26 stub

---
+++ src/main/scala/problems/p26/P26.scala
@@ -0,0 +1,12 @@
+// references:
+// 1. http://aperiodic.net/phil/scala/s-99/p26.scala
+
+package problems.p26
+
+object P26 {
+
+  def main(args: Array[String]): Unit = {
+  }
+
+  def combinations[A](k: Int, ls: List[A]): List[List[A]] = ???
+}
